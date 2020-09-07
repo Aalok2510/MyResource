f9280f8a056c582f78fdc46a2ed8f0101ddb5af2
Aalok2510 <43931968+Aalok2510@users.noreply.github.com>
2020-09-08 03:29:42 +0530

    Update linkparambroken.sh

--- BugBounty/Tools/linkparambroken.sh
+++ BugBounty/Tools/linkparambroken.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 shopt -s expand_aliases
 alias httpx=/home/hack2death/Tools/./httpx
+alias linkfinder='python3 /home/hack2death/Tools/LinkFinder/linkfinder.py-o cli -i '
 dir=/home/hack2death/Recon/$1
 mkdir  $dir;
 v=$(echo $1 | httpx) 
-python3 /home/hack2death/Tools/LinkFinder/linkfinder.py   -i $v -o cli >> $dir/$1_linkfinder.txt
+linkfinder $v >> $dir/$1_linkfinder.txt
 python3 /home/hack2death/Tools/ParamSpider/paramspider.py -d $v --subs False  -e  png,jpeg,ico,css,svg,gif,js -l high -o $dir/$1_paramspider.txt
 blc  $v   >>  $dir/$1_brokenlink.txt
 
