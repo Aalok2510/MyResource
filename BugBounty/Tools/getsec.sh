403af3edd2b92e445b5deb5193ed0524adb91486
Aalok2510 <43931968+Aalok2510@users.noreply.github.com>
2020-09-08 07:24:33 +0530

    Update getsec.sh

--- BugBounty/Tools/getsec.sh
+++ BugBounty/Tools/getsec.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 shopt -s expand_aliases
-alias httprobe=/home/hack2death/go/bin/./httprobe
+alias httpx=/home/hack2death/Tools/./httpx
+alias secretfinder='python3 /home/hack2death/Tools/secretfinder/SecretFinder.py -e -o cli -i'
 dir=/home/hack2death/Recon/$1
-v=$(echo $1 | httprobe -prefer-https) 
-python3 /home/hack2death/Tools/secretfinder/SecretFinder.py -i $v -e -o cli >  $dir/$1_secretfinder.txt;
+v=$(echo $1 | httpx) 
+secretfinder $v  >  $dir/$1_secretfinder.txt;
  
 
 
