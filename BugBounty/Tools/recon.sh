1d11980866569d6d2f2533efd4097482621ab138
Aalok2510 <43931968+Aalok2510@users.noreply.github.com>
2020-09-09 12:21:26 +0530

    Create recon.sh

---
+++ BugBounty/Tools/recon.sh
@@ -0,0 +1,30 @@
+                                                                                                   
+#!/bin/bash
+echo '
+/ ___/_________  _________  (_)___ _____ 
+  \__ \/ ___/ __ \/ ___/ __ \/ / __ `/ __ \
+ ___/ / /__/ /_/ / /  / /_/ / / /_/ / / / /
+/____/\___/\____/_/  / .___/_/\__,_/_/ /_/ 
+                    /_/                    
+'
+echo  ' Automated with <3 by Hack2Death'
+
+dir=/home/hack2death/arsenal 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+if [ $1 = "s" ]
+then
+  for url in $(cat $2); do
+     echo '_____________________________________________'
+     echo  "${red} Testing : ${green} ${url} ${reset}"
+     echo '---------------------------------------------'
+     echo '_________________________________________________________'
+     echo  "${red} Performing : ${green} Small Scope Recon ${reset}"
+     echo '---------------------------------------------------------'
+     $dir/smallscope.sh $url;   
+  done
+fi
+
+
